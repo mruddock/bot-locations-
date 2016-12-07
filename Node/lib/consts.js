7b2954e18646e23944a6bc5ec93f75ac8f40bfe4
Ibrahim Assal <iassal@microsoft.com>
2016-12-07 13:58:41 +0200

    Rolling back to latest released botbuilder version

--- Node/lib/consts.js
+++ Node/lib/consts.js
@@ -1,13 +1,13 @@
 "use strict";
 exports.LibraryName = "botbuilder-location";
 exports.Strings = {
-    "AskForCountry": "AskForCountry",
-    "AskForLocality": "AskForLocality",
-    "AskForPostalCode": "AskForPostalCode",
-    "AskForRegion": "AskForRegion",
-    "AskForStreetAddress": "AskForStreetAddress",
-    "InvalidLocationResponse": "InvalidLocationResponse",
-    "LocationNotFound": "LocationNotFound",
-    "MultipleResultsFound": "MultipleResultsFound",
-    "SingleResultFound": "SingleResultFound"
+    "AskForCountry": exports.LibraryName + ":AskForCountry",
+    "AskForLocality": exports.LibraryName + ":AskForLocality",
+    "AskForPostalCode": exports.LibraryName + ":AskForPostalCode",
+    "AskForRegion": exports.LibraryName + ":AskForRegion",
+    "AskForStreetAddress": exports.LibraryName + ":AskForStreetAddress",
+    "InvalidLocationResponse": exports.LibraryName + ":InvalidLocationResponse",
+    "LocationNotFound": exports.LibraryName + ":LocationNotFound",
+    "MultipleResultsFound": exports.LibraryName + ":MultipleResultsFound",
+    "SingleResultFound": exports.LibraryName + ":SingleResultFound"
 };
--- Node/src/consts.ts
+++ Node/src/consts.ts
@@ -2,13 +2,13 @@
 export const LibraryName = "botbuilder-location";
 
 export const Strings = {
-    "AskForCountry": "AskForCountry",
-    "AskForLocality": "AskForLocality",
-    "AskForPostalCode": "AskForPostalCode",
-    "AskForRegion": "AskForRegion",
-    "AskForStreetAddress": "AskForStreetAddress",
-    "InvalidLocationResponse": "InvalidLocationResponse",
-    "LocationNotFound": "LocationNotFound",
-    "MultipleResultsFound": "MultipleResultsFound",
-    "SingleResultFound": "SingleResultFound"
+    "AskForCountry": exports.LibraryName + ":AskForCountry",
+    "AskForLocality": exports.LibraryName + ":AskForLocality",
+    "AskForPostalCode": exports.LibraryName + ":AskForPostalCode",
+    "AskForRegion": exports.LibraryName + ":AskForRegion",
+    "AskForStreetAddress": exports.LibraryName + ":AskForStreetAddress",
+    "InvalidLocationResponse": exports.LibraryName + ":InvalidLocationResponse",
+    "LocationNotFound": exports.LibraryName + ":LocationNotFound",
+    "MultipleResultsFound": exports.LibraryName + ":MultipleResultsFound",
+    "SingleResultFound": exports.LibraryName + ":SingleResultFound"
 };
