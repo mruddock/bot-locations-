3f82a3c4e526a40cfe0a371052f181d782a7862e
Ibrahim Assal <iassal@microsoft.com>
2016-12-07 14:11:41 +0200

    Adding form augmentation to the urls

--- Node/src/services/bing-geospatial-service.ts
+++ Node/src/services/bing-geospatial-service.ts
@@ -1,10 +1,11 @@
 import * as rp from 'request-promise';
 import { sprintf } from 'sprintf-js';
 
-const findLocationByQueryUrl = "https://dev.virtualearth.net/REST/v1/Locations?";
-const findLocationByPointUrl = "https://dev.virtualearth.net/REST/v1/Locations/%1$s,%2$s?";
-const findImageByPointUrl = "https://dev.virtualearth.net/REST/V1/Imagery/Map/Road/%1$s,%2$s/15?mapSize=500,500&pp=%1$s,%2$s;1;%3$s&dpi=1";
-const findImageByBBoxUrl = "https://dev.virtualearth.net/REST/V1/Imagery/Map/Road?mapArea=%1$s,%2$s,%3$s,%4$s&mapSize=500,500&pp=%5$s,%6$s;1;%7$s&dpi=1";
+const formAugmentation = "&form=BTCTRL"
+const findLocationByQueryUrl = "https://dev.virtualearth.net/REST/v1/Locations?" + formAugmentation;
+const findLocationByPointUrl = "https://dev.virtualearth.net/REST/v1/Locations/%1$s,%2$s?" + formAugmentation;
+const findImageByPointUrl = "https://dev.virtualearth.net/REST/V1/Imagery/Map/Road/%1$s,%2$s/15?mapSize=500,500&pp=%1$s,%2$s;1;%3$s&dpi=1" + formAugmentation;
+const findImageByBBoxUrl = "https://dev.virtualearth.net/REST/V1/Imagery/Map/Road?mapArea=%1$s,%2$s,%3$s,%4$s&mapSize=500,500&pp=%5$s,%6$s;1;%7$s&dpi=1" + formAugmentation;
 
 export function getLocationByQuery(apiKey: string, address: string): Promise<Array<any>> {
     var url = addKeyToUrl(findLocationByQueryUrl, apiKey) + "&q=" + encodeURIComponent(address);
